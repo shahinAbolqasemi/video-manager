bbcff10e7ab5ef2edc51dadfdaaa367ccb946bef
spsr <shahin199501@yahoo.com>
2021-04-17 15:33:39 +0430

    add search and filter options to views

--- ticketing/views.py
+++ ticketing/views.py
@@ -1,49 +1,78 @@
 from django.shortcuts import render
 from base.views import BaseViewSet
-from .models import TicketCategory, TicketStatus, Ticket, TicketMessage, TicketFileAttachment, TicketComment, TicketCategoryUserAssignment, TicketCategoryReferRequest, UserReferRequest
-from .serializers import TicketCategorySerializer, TicketStatusSerializer, TicketSerializer, TicketMessageSerializer, TicketFileAttachmentSerializer, TicketCommentSerializer, TicketCategoryUserAssignmentSerializer, TicketCategoryReferRequestSerializer, UserReferRequestSerializer
+from .models import TicketCategory, TicketStatus, Ticket, TicketMessage, TicketFileAttachment, TicketComment, \
+    TicketCategoryUserAssignment, TicketCategoryReferRequest, UserReferRequest
+from .serializers import TicketCategorySerializer, TicketStatusSerializer, TicketSerializer, TicketMessageSerializer, \
+    TicketFileAttachmentSerializer, TicketCommentSerializer, TicketCategoryUserAssignmentSerializer, \
+    TicketCategoryReferRequestSerializer, UserReferRequestSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 
 class TicketCategoryViewSet(BaseViewSet):
     queryset = TicketCategory.objects.all()
     serializer_class = TicketCategorySerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['title']
+    search_fields = ['title']
 
 
 class TicketStatusViewSet(BaseViewSet):
     queryset = TicketStatus.objects.all()
     serializer_class = TicketStatusSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['title']
+    search_fields = ['title']
+
 
 
 class TicketViewSet(BaseViewSet):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['subject', 'related_priority', 'related_ticket_category', 'related_subscription',
+                        'related_ticket_status', 'referred_to_ticket_category', 'referred_to_user']
+    search_fields = ['subject']
+
 
 
 class TicketMessageViewSet(BaseViewSet):
     queryset = TicketMessage.objects.all()
     serializer_class = TicketMessageSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['related_ticket']
 
 
 class TicketFileAttachmentViewSet(BaseViewSet):
     queryset = TicketFileAttachment.objects.all()
     serializer_class = TicketFileAttachmentSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['related_ticket_message']
 
 
 class TicketCommentViewSet(BaseViewSet):
     queryset = TicketComment.objects.all()
     serializer_class = TicketCommentSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['related_ticket']
 
 
 class TicketCategoryUserAssignmentViewSet(BaseViewSet):
     queryset = TicketCategoryUserAssignment.objects.all()
     serializer_class = TicketCategoryUserAssignmentSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['related_ticket_category', 'related_user']
 
 
 class TicketCategoryReferRequestViewSet(BaseViewSet):
     queryset = TicketCategoryReferRequest.objects.all()
     serializer_class = TicketCategoryReferRequestSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['related_ticket', 'related_ticket_category']
 
 
 class UserReferRequestViewSet(BaseViewSet):
     queryset = UserReferRequest.objects.all()
     serializer_class = UserReferRequestSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['related_ticket', 'related_user']
