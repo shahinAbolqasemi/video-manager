3852d5f3356f1b12351e6c902e3c9287b2a9d240
spsr <shahin199501@yahoo.com>
2021-04-20 12:53:40 +0430

    modify service-field-assignment to service-field-assignments

--- plan/urls.py
+++ plan/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
 from .views import ServiceViewSet, SubscriptionViewSet, SubscriptionServiceAssignmentViewSet, ServiceFieldViewSet, \
-    SubscriptionServiceAssignmentFieldViewSet, ServiceFieldAssignmentViewSet
+    SubscriptionServiceAssignmentFieldViewSet, ServiceFieldAssignmentViewSet, SubscriptionAssignmentViewSet
 
 router = SimpleRouter()
 router.register(r'services', ServiceViewSet)
@@ -9,6 +9,7 @@ router.register(r'subscription', SubscriptionViewSet)
 router.register(r'subscription-service-assignments', SubscriptionServiceAssignmentViewSet)
 router.register(r'service-fields', ServiceFieldViewSet)
 router.register(r'subscription-service-assignment-fields', SubscriptionServiceAssignmentFieldViewSet)
-router.register(r'service-field-assignment', ServiceFieldAssignmentViewSet)
+router.register(r'service-field-assignments', ServiceFieldAssignmentViewSet)
+router.register(r'subscription-assignments', SubscriptionAssignmentViewSet)
 
 urlpatterns = router.urls
