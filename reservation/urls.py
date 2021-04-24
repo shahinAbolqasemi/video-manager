4e0b22f67adc12cd1688fbdb3bbb4dbb54338254
spsr <shahin199501@yahoo.com>
2021-04-24 15:46:30 +0430

    Change Session keyword to Service

--- reservation/urls.py
+++ reservation/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ServiceRequestViewSet, ParticipantViewSet, ParticipantAssignmentViewSet, \
-    SessionRequestServiceFieldAssignmentViewSet
+    ServiceRequestServiceFieldAssignmentViewSet
 
 router = DefaultRouter()
 router.register(r'service-requests', ServiceRequestViewSet, basename='session_request')
 router.register(r'participants', ParticipantViewSet, basename='participants')
 router.register(r'participant-assignments', ParticipantAssignmentViewSet, basename='participant_assignments')
-router.register(r'session-request-service-field-assignments', SessionRequestServiceFieldAssignmentViewSet,
+router.register(r'service-request-service-field-assignments', ServiceRequestServiceFieldAssignmentViewSet,
                 basename='session_request_service_field_assignment')
 
 urlpatterns = [
