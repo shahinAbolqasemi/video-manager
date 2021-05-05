68f2503fd698e1ad53c2dc1dffe8dfca05d216c8
Shahin Abolqasemi <shahin.abolqasemi@yahoo.com>
2021-05-05 16:02:20 +0430

    add uuid to user model

--- base/models.py
+++ base/models.py
@@ -1,8 +1,19 @@
+import uuid as uuid
+from django.db import models
 from django.db.models import Model, DateTimeField, BooleanField, CharField, ForeignKey, SET_NULL, Index
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    id = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    uuid = models.UUIDField(
+        verbose_name='شناسه منحصر به فرد کاربر',
+        primary_key=False,
+        default=uuid.uuid4,
+        editable=False,
+        unique=True,
+        help_text='unique id for users'
+    )
 
     def __str__(self):
         return self.username
